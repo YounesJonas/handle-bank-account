56ea658bd25ac658f81487d27ecbf53cb1986207
YOUNES BOUAZIZI <bouazizi.younes@gmail.com>
2022-09-02 11:29:34 +0100

    initialization of the project

---
+++ src/main/java/fr/societegenerale/handleBankAccount/HandleBankAccountApplication.java
@@ -0,0 +1,13 @@
+package fr.societegenerale.handleBankAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleBankAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/societegenerale/handleBankAccount/HandleBankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.societegenerale.handleBankAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleBankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
